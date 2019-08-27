4f4ca04ad39849a115512053a8801cd25c5df435
alzack67 <bartoszkaszuba1@gmail.com>
2019-08-27 18:12:46 +0200

    Fixing mean mistake
    Fixing minor mistake with calculating mean

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -9,30 +9,33 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private int trialCount;
-    private double mean;
-    private double stddev;
+
+    // private final double [] resultTab;
+    private final int trialCount;
+    private final double mean;
+    private final double stddev;
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
-            throw new java.lang.IllegalArgumentException("Neither size of grid nor trial count cannot be a value equal to or below zero");
+            throw new IllegalArgumentException("Neither size of grid nor trial count cannot be a value equal to or below zero");
         }
         trialCount = trials;
-        int [] resultTab = new int[trials];
+        double [] resultTab = new double[trials];
         for (int i = 0; i < trials; i++)
         {
             Percolation percolation = new Percolation(n);
             while (!percolation.percolates()) {
                 percolation.open(StdRandom.uniform(1, n+1), StdRandom.uniform(1, n+1));
             }
-            resultTab[i] = percolation.numberOfOpenSites();
+            resultTab[i] = (percolation.numberOfOpenSites())/(n*n*1.0);
         }
-        mean =  StdStats.mean(resultTab);
+        mean = StdStats.mean(resultTab);
         stddev = StdStats.stddev(resultTab);
     }
 
     // sample mean of percolation threshold
-    public double mean() { return mean;
+    public double mean() {
+        return mean;
     }
 
     // sample standard deviation of percolation threshold
@@ -42,17 +45,18 @@ public class PercolationStats {
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - (1.96*stddev())/Math.sqrt(trialCount);
+        return mean - (1.96*stddev)/Math.sqrt(trialCount);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + (1.96*stddev())/Math.sqrt(trialCount);
+        return mean + (1.96*stddev)/Math.sqrt(trialCount);
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        PercolationStats pstats = new PercolationStats(10, 20);
+        System.out.println(pstats.mean());
     }
 
 }
